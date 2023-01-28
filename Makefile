9e93cef65d129084b02ec4e68e951d44918c693a
Benjamin Howard <bentlife@icloud.com>
2023-01-27 21:14:30 -0800

    Change Makefile back to original Makefile.

--- Makefile
+++ Makefile
@@ -1,14 +1,11 @@
-# TODO: Change back to original make file.
+
 ifdef USE_INT
 MACRO = -DUSE_INT
 endif
 
 #compiler setup
 CXX = g++
-# TODO: Restore original flags.
-# CXXFLAGS = -std=c++14 -O3 -pthread $(MACRO)
-CXXFLAGS = -std=c++14 -Og -pthread $(MACRO) -Wall -Werror -Wno-error=unknown-pragmas
-
+CXXFLAGS = -std=c++14 -O3 -pthread $(MACRO)
 
 COMMON= core/utils.h core/cxxopts.h core/get_time.h core/graph.h core/quick_sort.h
 SERIAL= pi_calculation triangle_counting page_rank
--- triangle_counting_parallel.cpp
+++ triangle_counting_parallel.cpp
@@ -162,8 +162,7 @@ int main(int argc, char *argv[])
                      cxxopts::value<uint>()->default_value(DEFAULT_NUMBER_OF_WORKERS)},
                     {"inputFile", "Input graph file path",
                      cxxopts::value<std::string>()->default_value(
-                    // TODO: Add back in!         "/scratch/input_graphs/roadNet-CA")},
-                    "/home/ben/Documents/SFU/cmpt-431/assignments/1/default_page_rank_graph/roadNet-CA")},
+                    "/scratch/input_graphs/roadNet-CA")},
             });
 
     auto cl_options = options.parse(argc, argv);
